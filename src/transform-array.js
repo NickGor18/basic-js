2803aaa3c38b17e0f8590652ebe1cf15044f7584
NickGor18 <44360550+NickGor18@users.noreply.github.com>
2020-03-14 21:19:58 +0300

    feat: solved

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,24 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if( !Array.isArray(arr) ) throw new Error('input is not array');
+
+  let result = [];
+
+  for( let i = 0; i < arr.length; i++) {
+    if(
+      arr[i] !== '--discard-next' 
+      && (i === 0 || arr[i-1] !== '--discard-next')
+      && arr[i] !== '--discard-prev' 
+      && arr[i] !== '--double-next' 
+      && arr[i] !== '--double-prev') { 
+        result.push(arr[i]);
+    } else if( arr[i] == '--discard-prev' ) { //ok
+        if( i > 0 ) { result.pop() };
+    } else if( arr[i] == '--double-next') { //ok
+        if(arr.length - 1 > i)
+      { result.push(arr[i+1]) };
+    } else if( arr[i] == '--double-prev') { //ok
+      if( i > 0 ) { result.push(arr[i-1]) };
+    };
+  };
+  return result;
 };
