bdbb00dffac087ef3b1ee4d73391d7e364761ceb
NickGor18 <44360550+NickGor18@users.noreply.github.com>
2020-03-14 21:12:42 +0300

    feat: solved

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,23 +1,35 @@
 const chainMaker = {
+  chain : [],
   getLength() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    return this.chain.length;
   },
   addLink(value) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    let chainElement = `( ${value} )`;
+    this.chain.push(chainElement);
+    return this;
   },
   removeLink(position) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    if (typeof position !== 'number' || position < 1 || position > this.getLength()) {
+      this.chain = [];
+      throw new Error('You try to remowe wrong link!');
+    }
+    if (position === 1) {
+      this.chain.shift();
+    } else if (position === this.getLength()) {
+      this.chain.pop();
+    } else {
+      this.chain.splice(position - 1, 1);
+    }
+    return this;
   },
   reverseChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
   finishChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    let result = this.chain.join('~~');
+    this.chain = [];
+    return result;
   }
 };
 
