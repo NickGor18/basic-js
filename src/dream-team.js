d41eb4b97847e0e5e8e094f4f66791252f7272be
NickGor18 <44360550+NickGor18@users.noreply.github.com>
2020-03-14 21:09:10 +0300

    feat: solved

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,13 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false;
+  }
+  const regExp = /[a-zA-Z]{1,1}/;
+  const filtered = members.filter((member) => typeof member === 'string');
+  const FIRST_CHARS = filtered.map((member) => {
+    const firstLetter = member.match(regExp)[0];
+    return firstLetter.toUpperCase();
+  });
+
+  return FIRST_CHARS.sort().join('');;
 };
