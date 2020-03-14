71412a22959c30d2266e89475ce5a53de590273a
NickGor18 <44360550+NickGor18@users.noreply.github.com>
2020-03-14 21:09:56 +0300

    feat: solved

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,44 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+    let {
+        repeatTimes,
+        separator,
+        addition = '',
+        additionRepeatTimes,
+        additionSeparator = ''
+    } = options;
+
+    if (!repeatTimes) {
+        return `${str}${addition}`;
+    }
+    separator = getSeparator(repeatTimes, separator);
+    let newAdditional = getAdditional(addition, additionSeparator, additionRepeatTimes);
+    let result = `${str}${newAdditional}${separator}`.repeat(repeatTimes);
+    let indexToTrim = result.length - separator.length;
+
+    return `${result.slice(0, indexToTrim)}`;
 };
-  
+
+
+
+function getSeparator(repeatTimes, separator) {
+    let result;
+
+    if (repeatTimes < 2) {
+        return '';
+    }
+
+    if (repeatTimes > 1 && !separator) {
+        return '+';
+    }
+
+    return separator;
+}
+
+function getAdditional(addition, additionSeparator, additionRepeatTimes) {
+    if (additionRepeatTimes === 1) {
+        return addition;
+    }
+
+    return `${addition}${additionSeparator}`.repeat(additionRepeatTimes).slice(0, -additionSeparator.length);
+
+}
